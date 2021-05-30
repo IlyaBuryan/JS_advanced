1c5c336f90821c5c2488a4cb1ec6a7ed1990b4f6
Ilya <forwow21@mail.ru>
2021-05-30 16:06:41 +0300

    Lesson6_HW

---
+++ Lesson6_HW/eshop/src/components/Basket.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="cart cart-list" v-show="isVisibleCart">
+    <div v-if="basketGoods.length > 0">
+      <div
+        v-for="item in basketGoods"
+        :key="item.id_product"
+        class="goods-item"
+      >
+        <h3>{{ item.product_name }}</h3>
+        <p>{{ item.price }}</p>
+        <button v-on:click="$emit('remove-basket', item.product_name)">
+          Удалить
+        </button>
+      </div>
+    </div>
+    <h2 v-else>Корзина пуста</h2>
+  </div>
+</template>
+
+
+<script>
+export default {
+  props: {
+    basketGoods: {
+      type: Array,
+      default: () => [],
+    },
+    isVisibleCart: {
+      type: Boolean,
+    },
+  },
+};
+</script>
+
+
+<style>
+.cart {
+  box-shadow: 0px 0px 8px 2px rgba(34, 60, 80, 0.2);
+  padding: 20px;
+  width: 800px;
+  height: 500px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: white;
+  overflow: auto;
+}
+.cart-list {
+  display: flex;
+  flex-flow: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+</style>
