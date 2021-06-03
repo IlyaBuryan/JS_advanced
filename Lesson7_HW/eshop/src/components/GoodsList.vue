9f5005cae5bdbaca2f2070c653a6766da19c4f36
Ilya <forwow21@mail.ru>
2021-06-03 12:48:54 +0300

    Lesson7_HW

---
+++ Lesson7_HW/eshop/src/components/GoodsList.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="goods-list">
+      <div v-for="item in goods" :key="item.id_product" class="goods-item">
+        <h3>{{ item.product_name }}</h3>
+        <p>{{ item.price }}</p>
+        <button v-on:click="$emit('add-basket', item)">Добавить</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  props: {
+    goods: {
+      type: Array,
+      default: () => [],
+    },
+  },
+};
+</script>
+
+<style>
+.goods-list {
+  display: flex;
+  flex-flow: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+.goods-item {
+  width: 200px;
+  height: 300px;
+  padding: 5px;
+  margin: 10px;
+  box-shadow: 0px 0px 8px 2px rgba(34, 60, 80, 0.2);
+}
+</style>
